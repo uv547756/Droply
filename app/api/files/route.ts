a296d257b7fc44b0f125953c9287c429daa916f4
Komi <uv47756@gmail.com>
2025-06-08 02:45:40 +0530

    added api feature retrieving files

---
+++ app/api/files/route.ts
@@ -0,0 +1,63 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { eq, and, or, isNull } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    try {
+        const {userId} = await auth()
+        if(!userId){
+            return NextResponse.json(
+                {error: "Unauthorized"},
+                {status: 401}
+            );
+        }
+
+        // ways the user might request a folder?
+
+        // query params are like: ?name=komi&id=10
+        const searchParams = request.nextUrl.searchParams
+        const queryUserId = searchParams.get("userId")
+        const parentId = searchParams.get("parentId")
+
+        if (!queryUserId || queryUserId !== userId) {
+            return NextResponse.json(
+                {error: "Unauthorized"},
+                {status: 401}
+            );
+        }
+
+        //fetch files from db
+        let userFiles;
+        if(parentId){
+            //fetching from a specific folder
+            const userFiles = await db
+            .select()
+            .from(files)
+            .where(
+                and(
+                    eq(files.userId, userId),
+                    eq(files.parentId, parentId)
+                )
+            )
+        } else {
+            await db
+                .select()
+                .from(files)
+                .where(
+                    and(
+                        eq(files.userId, userId),
+                        isNull(files.parentId)   
+                    )
+                )
+        }
+        
+        return NextResponse.json(userFiles)
+    } catch (error){
+        return NextResponse.json(
+            {error: "Error fetching files"},
+            {status: 500}
+        )     
+    }
+}
