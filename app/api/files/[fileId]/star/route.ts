80ad292dfd18e27c48bbba499789408ef6a8dccd
Komi <uv47756@gmail.com>
2025-06-08 03:43:12 +0530

    API feature to make a file(s) as starred

---
+++ app/api/files/[fileId]/star/route.ts
@@ -0,0 +1,61 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+    request:NextRequest,
+    props: {params: Promise<{fileId: string}>}
+) {
+    try {
+        const {userId} = await auth();
+        if (!userId){
+            return NextResponse.json(
+                {error: "Unauthorized"},
+                {status: 401}
+            );
+        }
+
+        // exactly same as [fileId]
+         const {fileId} = await props.params
+
+        if (!fileId){
+            return NextResponse.json(
+                {error: "File ID is required"},
+                {status: 401}
+            );
+        }
+
+        const [file] = await db.select().from(files).where(
+            and(
+                eq(files.id, fileId),
+                eq(files.userId, userId)
+            )
+        )
+
+        if (!file){
+            return NextResponse.json(
+                {error: "File not found"},
+                {status: 401}
+            );
+        }
+
+        //toggle the starred status
+        const updatedFiles = await db.update(files).set({isStarred: true}).where(
+            and(
+                eq(files.id, fileId),
+                eq(files.userId, userId)
+            )
+        ).returning();
+
+        console.log("File updated: ", updatedFiles);
+        const updatedFile = updatedFiles[0]
+
+        return NextResponse.json(updatedFile)
+
+    } catch (error) {
+
+    }
+    
+}
